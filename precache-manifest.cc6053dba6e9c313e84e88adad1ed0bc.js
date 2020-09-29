c35a35e0bbd85ace8d1c0ed16b6f9be4e86cce22
Frida <frida.johansson888@gmail.com>
2020-09-29 09:37:18 +0200

    Updates

---
+++ precache-manifest.cc6053dba6e9c313e84e88adad1ed0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfa03ac770e395d77d56a84494f2771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d5f0ac0fd862b2dda29",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "16632308aaf91cd6e362",
+    "url": "/static/js/2.23cb5d0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23cb5d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5f0ac0fd862b2dda29",
+    "url": "/static/js/main.9b2031c7.chunk.js"
+  },
+  {
+    "revision": "488db97cd057b7955bcb",
+    "url": "/static/js/runtime-main.d43152b0.js"
+  }
+]);
